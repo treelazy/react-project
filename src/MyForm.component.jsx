2603e645173f1ece5e8bca30dac358bba2ee2310
資訊研發-韓書恒 <henry.han@udnshopping.com>
2021-06-17 17:51:50 +0800

    feat: connect input controls to form's state

--- src/MyForm.component.jsx
+++ src/MyForm.component.jsx
@@ -9,21 +9,99 @@ import {
   Checkbox,
   Select,
   DatePicker,
-  Row,
 } from "antd";
 
+const COUNTRIES = [
+  { name: "China", value: "china" },
+  { name: "U.S.A", value: "usa" },
+];
+const COLORS = [
+  { name: "Red", value: "red" },
+  { name: "Green", value: "green" },
+  { name: "Blue", value: "blue" },
+];
+const RACES = [
+  { name: "Asian", value: "asian" },
+  { name: "African", value: "african" },
+  { name: "Caucasian", value: "caucasian" },
+];
+const FOODS = [
+  { name: "Rice", value: "rice" },
+  { name: "Noodle", value: "noodle" },
+  { name: "Meat", value: "meat" },
+];
+
 export default class MyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      country: "",
+      colors: [],
+      race: "",
+      isSwitched: false,
+      foods: [],
+      date: null,
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSingleSelect = this.handleSingleSelect.bind(this);
+    this.handleMultipleSelect = this.handleMultipleSelect.bind(this);
+    this.handleRadio = this.handleRadio.bind(this);
+    this.handleSwitch = this.handleSwitch.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.handleDate = this.handleDate.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleSingleSelect(value) {
+    this.setState({ country: value });
+  }
+
+  handleMultipleSelect(values) {
+    this.setState({ colors: values });
+  }
+
+  handleRadio(e) {
+    this.setState({ race: e.target.value });
+  }
+
+  handleSwitch(e) {
+    this.setState((curState) => ({ isSwitched: !curState.isSwitched }));
+  }
+
+  handleCheckbox(values) {
+    this.setState({ foods: values });
+  }
+
+  handleDate(value) {
+    this.setState({ date: value });
+  }
+
+  handleReset() {
+    console.log("reset");
+  }
+
+  handleSubmit() {
+    console.log("submit");
+  }
+
   render() {
+    const { name, country, colors, race, isSwitched, foods, date } = this.state;
     return (
-      <Row justify="center">
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Col span={12}>
-          <Form
-            labelCol={{ span: 12 }}
-            wrapperCol={{ span: 12 }}
-          >
+          <Form labelCol={{ span: 6 }} wrapperCol={{ span: 14 }}>
             <h1 style={{ textAlign: "center" }}>MyForm</h1>
             <Form.Item label="Name">
-              <Input placeholder="Please enter your name" />
+              <Input
+                placeholder="Please enter your name"
+                value={name}
+                onChange={this.handleNameChange}
+              />
             </Form.Item>
             <Form.Item
               name="country"
@@ -36,82 +114,82 @@ export default class MyForm extends Component {
                 },
               ]}
             >
-              <Select placeholder="Please select a country">
-                <Select.Option value="china">China</Select.Option>
-                <Select.Option value="usa">U.S.A</Select.Option>
+              <Select
+                placeholder="Please select a country"
+                onChange={this.handleSingleSelect}
+                value={country}
+              >
+                {COUNTRIES.map((country) => (
+                  <Select.Option key={country.value} value={country.value}>
+                    {country.name}
+                  </Select.Option>
+                ))}
               </Select>
             </Form.Item>
             <Form.Item name="color" label="Color">
               <Select
                 mode="multiple"
                 placeholder="Please select favourite colors"
+                onChange={this.handleMultipleSelect}
+                value={colors}
               >
-                <Select.Option value="red">Red</Select.Option>
-                <Select.Option value="green">Green</Select.Option>
-                <Select.Option value="blue">Blue</Select.Option>
+                {COLORS.map((color) => (
+                  <Select.Option key={color.value} value={color.value}>
+                    {color.name}
+                  </Select.Option>
+                ))}
               </Select>
             </Form.Item>
             <Form.Item name="race" label="Race">
-              <Radio.Group>
-                <Radio value="a">Asian</Radio>
-                <Radio value="b">African</Radio>
-                <Radio value="c">Caucasian</Radio>
+              <Radio.Group onChange={this.handleRadio} value={race}>
+                {RACES.map((race) => (
+                  <Radio key={race.value} value={race.value}>
+                    {race.name}
+                  </Radio>
+                ))}
               </Radio.Group>
             </Form.Item>
             <Form.Item name="switch" label="Switch" valuePropName="checked">
-              <Switch />
+              <Switch checked={isSwitched} onChange={this.handleSwitch} />
             </Form.Item>
             <Form.Item name="food" label="Food">
-              <Checkbox.Group>
-                <Col span={8}>
+              <Checkbox.Group value={foods} onChange={this.handleCheckbox}>
+                {FOODS.map((food) => (
                   <Checkbox
-                    value="A"
+                    key={food.value}
+                    value={food.value}
                     style={{
                       lineHeight: "32px",
                     }}
                   >
-                    Rice
+                    {food.name}
                   </Checkbox>
-                </Col>
-                <Col span={8}>
-                  <Checkbox
-                    value="B"
-                    style={{
-                      lineHeight: "32px",
-                    }}
-                  >
-                    Noodle
-                  </Checkbox>
-                </Col>
-                <Col span={8}>
-                  <Checkbox
-                    value="C"
-                    style={{
-                      lineHeight: "32px",
-                    }}
-                  >
-                    Meat
-                  </Checkbox>
-                </Col>
+                ))}
               </Checkbox.Group>
             </Form.Item>
             <Form.Item label="Date">
-              <DatePicker />
+              <DatePicker onChange={this.handleDate} value={date}/>
             </Form.Item>
             <Form.Item
               wrapperCol={{
                 span: 12,
-                offset: 12,
+                offset: 6,
               }}
             >
-              <Button type="primary" htmlType="submit">
+              <Button
+                type="primary"
+                htmlType="submit"
+                onClick={() => {
+                  console.log("test");
+                }}
+              >
                 Submit
               </Button>
               <Button htmlType="button">Reset</Button>
             </Form.Item>
           </Form>
         </Col>
-      </Row>
+      </div>
     );
   }
 }
